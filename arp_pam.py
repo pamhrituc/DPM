13e4ad557d360924a807acf08844c76881bc9a36
pamthespam <hpie2332@scs.ubbcluj.ro>
2020-04-23 19:43:19 +0300

    ARP traffic can be detectedgit add .!

--- arp_pam.py
+++ arp_pam.py
@@ -29,18 +29,21 @@ def get_arp_cache(interface):
             ip_match = re.search('[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}', element)
             mac_match = re.search('[0-9a-f]{2}[:-][0-9a-f]{2}[:-][0-9a-f]{2}[:-][0-9a-f]{2}[:-][0-9a-f]{2}[:-][0-9a-f]{2}', element)
             interface_match = re.search(interface, element)
-            if ip_match != None and mac_match != None and interface_match != None:
-                arp_cache.append((ip_match.group(0), mac_match.group(0)))
+            arp_entry_type = re.search('dynamic', element)
+            if ip_match != None and mac_match != None and (interface_match != None or arp_entry_type != None):
+                if (ip_match.group(0), mac_match.group(0)) not in arp_cache:
+                    arp_cache.append((ip_match.group(0), mac_match.group(0)))
     print(arp_cache)
     return
 
 
 def monitor(packet):
-
-    if packet[ARP].op == "is-at":
+    #Check for 2, since this represents a reply
+    if packet[ARP].op == 2:
+        print("YEET")
         try:
-            real_mac = get_mac(packet[ARP].psrc)
-            response_mac = get_mac(packet[ARP].hwsrc)
+            real_mac = getmac.get_mac_address(ip = packet[ARP].psrc)
+            response_mac = getmac.get_mac_address(ip = packet[ARP].hwsrc)
 
             if real_mac != response_mac:
                 print("[!] An ARP Poisoning attack is being attempted. Real MAC: %s. Response MAC: %s." % (real_mac, response_mac))
@@ -56,7 +59,7 @@ except:
     sys.exit(0)
 
 #set the interface
-conf.iface = interface
+#conf.iface = interface
 
 #turn off output
 conf.verb = 0
@@ -86,4 +89,4 @@ else:
     print ("[*] Gateway %s is at %s" % (gateway_ip, gateway_mac))
 
 get_arp_cache(interface)
-#sniff(filter = "arp", prn = monitor)
+sniff(filter = "arp", prn = monitor)
