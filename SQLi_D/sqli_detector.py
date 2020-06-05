84c32e1f77d8295a0ae5eddb5913f18a8c6de107
Pamela Ottilia Hrituc <44650892+pamhrituc@users.noreply.github.com>
2020-06-05 16:30:06 +0300

    Displays input parameters on a url given as parameter

--- SQLi_D/sqli_detector.py
+++ SQLi_D/sqli_detector.py
@@ -1,6 +1,7 @@
 from colorama import init, Fore
 from scapy.all import *
 from scapy.layers.http import HTTPRequest, HTTPResponse
+import argparse
 import datetime
 import os
 import requests
@@ -14,50 +15,57 @@ RED = Fore.RED
 RESET = Fore.RESET
 
 def return_fields(text):
+    input_types = ["email", "file", "hidden", "image", "password", "search", "tel", "text", "url"]
     fields = []
-    unprocessed_fields = re.findall("name=[a-zA-Z0-9]+", text)
-    for field in unprocessed_fields:
-        fields.append(field[field.find('=') + 1:])
+    unprocessed_input_fields = re.findall("<input [a-zA-Z0-9=\" \' /]+>", text)
+    for i in range(len(unprocessed_input_fields)):
+        for input_type in input_types:
+            if input_type in unprocessed_input_fields[i]:
+                if "name" in unprocessed_input_fields[i]:
+                    temp = re.findall("name ?= ?[\"\']?[a-zA-Z0-9]+[\"\']?", unprocessed_input_fields[i])[0]
+                    fields.append(temp[temp.find('=') + 2:-1])
+                elif "id" in unprocessed_input_fields[i]:
+                    temp = re.findall("id ?= ?[\"\']?[a-zA-Z0-9]+[\"\']?", unprocessed_input_fields[i])[0]
+                    fields.append(temp[temp.find('=') + 2 : -1])
+                continue
 
     return fields
 
 def process_packet(packet):
+    global url
+    global fields
     now = datetime.datetime.now()
     if packet[TCP].payload:
         data_packet = str(packet[TCP].payload)
-        url = data_packet[data_packet.find('Referer: ') + len('Referer: '):]
-        url = url[:url.find("\\r\\n")]
-        print(f"1: {packet[TCP].payload}")
-        if packet.haslayer(HTTPRequest):
-            src_ip = packet[IP].src
-            if src_ip != host_ip:
-                if packet[HTTPRequest].Host != None:
-                    url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
-                    method = packet[HTTPRequest].Method.decode()
-                    print(f"\n{GREEN}[*] {src_ip} requested {url} with {method}.{RESET}")
-                    print(f"\n{BLUE}[{now.hour}:{now.minute}:{now.second}] We just got a request!{RESET}")
-                    if method == 'POST':
-                        try:
-                            if 'http' not in url:
-                                url = 'http://' + url
-                            fields = return_fields(requests.get(url).text)
-                            print(f"2: {data_packet}")
-                            for field in fields:
-                                if field in data_packet.lower():
-                                    print(f"3: {data_packet}")
-                            print(f"4: {data_packet}")
-                        except Exception as e:
-                            print(e)
-
+        src_ip = packet[IP].src
+        if src_ip != host_ip:
+            print(f"\n{GREEN}[{now.hour}:{now.minute}:{now.second}] {src_ip} requested {url}.{RESET}")
+            print(f"\n{BLUE}[{now.hour}:{now.minute}:{now.second}] We just got a request!{RESET}")
+            try:
+                for field in fields:
+                    if field in data_packet.lower():
+                        print(f"3: {data_packet}")
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "")
+    parser.add_argument("-u", "--url", help = "The url to track after SQLi or XSS", required = True)
+    args = parser.parse_args()
+    if args.url:
+        url = args.url
+
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
         host_ip = s.getsockname()[0]
         print(f"\n{BLUE}[*] Your host ip: {host_ip}.{RESET}")
 
+        if 'http' not in url:
+            url = 'http://' + url
+
+        fields = return_fields(requests.get(url).text)
         sniff(filter = 'tcp', prn = process_packet, store = False)
 
     except KeyboardInterrupt:
